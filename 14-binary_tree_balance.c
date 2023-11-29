5e1300c6e0712aa33ba90150038a5e0a4101c76d
setalxswe <setalxswe@gmail.com>
2023-11-29 14:48:11 -0800

    lr

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node
+ * A NULL pointer is not a leaf
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leftleaves, rightleaves;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	leftleaves = binary_tree_leaves(tree->left);
+
+	rightleaves = binary_tree_leaves(tree->right);
+
+	return (leftleaves + rightleaves);
+}
