72839c029156786430295303ec893e2386c3fb69
setalxswe <setalxswe@gmail.com>
2023-11-29 14:07:14 -0800

    root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left != NULL && node->right != NULL)
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	else
 		return(0);
