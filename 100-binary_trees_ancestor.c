cc3ee0ad2d4f60b8efa3a8f99c11fe7b7b146ac4
setalxswe <setalxswe@gmail.com>
2023-11-29 16:00:09 -0800

    100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the LCA || NULL on failure
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	const binary_tree_t *leftLCA, *rightLCA;
+
+	for (leftLCA = first; leftLCA; leftLCA = leftLCA->parent)
+		for (rightLCA = second; rightLCA; rightLCA = rightLCA->parent)
+			if (leftLCA == rightLCA)
+				return ((binary_tree_t *)leftLCA);
+	
+	return (NULL);
+}
